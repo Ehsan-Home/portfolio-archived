8b96ed15096c4ed3e7db49fc7e7d4beef5a7b85b
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-05-17 10:42:07 -0700

    footer updated

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
-  locale_region: 'en-us',
+  site_name: 'Ehsan Ghasaei',
+  title: 'Ehsan Ghasaei',
+  description: 'Ehsan Ghasaei portfolio website',
+  author: 'Ehsan',
+  locale_region: 'en-ca',
   locale: 'en',
 };
