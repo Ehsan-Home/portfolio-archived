e5dab47170ee4c5ec00a5a9a7ea961a353319ff7
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-05-17 19:18:58 -0700

    progress bar design completed

--- src/partials/ProgressBar.tsx
+++ src/partials/ProgressBar.tsx
@@ -5,22 +5,18 @@ export interface ProgressBarProps {
 
 const ProgressBar = (props: ProgressBarProps) => {
   return (
-    <>
-      <div className="mb-1 flex justify-between">
-        <span className="text-base font-medium text-white dark:text-white">
-          {props.name}
-        </span>
-        <span className="text-sm font-medium text-blue-700 dark:text-white">
-          {props.prec}
-        </span>
-      </div>
-      <div className="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+    <div className="w-1/2 p-3">
+      <div>{props.name}</div>
+      <div className="w-full rounded-full bg-gray-200 dark:bg-gray-700">
         <div
-          className="h-2.5 rounded-full bg-blue-600"
+          className="rounded-full bg-cyan-600 p-0.5 text-center text-xs font-medium leading-none text-blue-100"
           style={{ width: `${props.prec}%` }}
-        ></div>
+        >
+          {' '}
+          {props.prec}%
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/partials/Skills.tsx
+++ src/partials/Skills.tsx
@@ -5,8 +5,47 @@ import ProgressBar from './ProgressBar';
 const Skills = () => {
   return (
     <Section title={<GradientText>Skills</GradientText>}>
-      <div className="flex flex-col items-center gap-x-8 rounded-md bg-slate-800 p-3 md:flex-row">
-        <ProgressBar name="React" prec={90} />
+      <div className="rounded-md bg-slate-800 p-3 ">
+        <h5 className="text-xl font-bold dark:text-white">Frontend</h5>
+        <div className="flex flex-wrap md:flex-row">
+          <ProgressBar name="JavaScript" prec={90} />
+          <ProgressBar name="TypeScript" prec={80} />
+          <ProgressBar name="React" prec={80} />
+          <ProgressBar name="Solid" prec={75} />
+          <ProgressBar name="HTML" prec={90} />
+          <ProgressBar name="CSS" prec={70} />
+          <ProgressBar name="Ant-design" prec={95} />
+          <ProgressBar name="Bootstrap" prec={90} />
+          <ProgressBar name="NextJS" prec={70} />
+          <ProgressBar name="tRPC" prec={60} />
+          <ProgressBar name="NPM" prec={80} />
+          <ProgressBar name="Vite" prec={70} />
+        </div>
+
+        <h5 className="text-xl font-bold dark:text-white">Backend</h5>
+        <div className="flex flex-wrap md:flex-row">
+          <ProgressBar name="Python" prec={80} />
+          <ProgressBar name="Django" prec={75} />
+          <ProgressBar name="AWS" prec={70} />
+          <ProgressBar name="SQL" prec={70} />
+          <ProgressBar name="NO-SQL" prec={60} />
+        </div>
+
+        <h5 className="text-xl font-bold dark:text-white">Tech</h5>
+        <div className="flex flex-wrap md:flex-row">
+          <ProgressBar name="Blockchain" prec={90} />
+          <ProgressBar name="Git" prec={80} />
+          <ProgressBar name="Rest API" prec={85} />
+          <ProgressBar name="Agile" prec={80} />
+          <ProgressBar name="Scrum" prec={70} />
+          <ProgressBar name="Linux" prec={70} />
+        </div>
+
+        <h5 className="text-xl font-bold dark:text-white">DevOps</h5>
+        <div className="flex flex-wrap md:flex-row">
+          <ProgressBar name="Docker" prec={70} />
+          <ProgressBar name="GitHub Actions" prec={60} />
+        </div>
       </div>
     </Section>
   );
